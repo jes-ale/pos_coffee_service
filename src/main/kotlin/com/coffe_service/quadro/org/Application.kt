19529cd6847452dab1441a8c1c0cf914aebabdd9
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2023-06-19 16:48:17 -0700

    file config server

--- src/main/kotlin/com/coffe_service/quadro/org/Application.kt
+++ src/main/kotlin/com/coffe_service/quadro/org/Application.kt
@@ -1,15 +1,9 @@
 package com.coffe_service.quadro.org
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.coffe_service.quadro.org.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     configureSockets()
     configureSerialization()
