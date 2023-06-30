2322bbe164bc76305edf620fa7dd234aa4de8e0c
Aybancho <ivan.rosas@uabc.edu.mx>
2023-06-30 20:34:23 +0000

    set next manual

--- src/main/kotlin/com/coffee_service/quadro/org/routes/ProductionRoutes.kt
+++ src/main/kotlin/com/coffee_service/quadro/org/routes/ProductionRoutes.kt
@@ -14,35 +14,39 @@ import java.util.stream.Collectors
 import kotlinx.serialization.Serializable
 
 object ProductionCache {
-    private val productionCache = mutableMapOf<String, List<ProductionPayload>>()
-    private val productionQueue = mutableListOf<String>()
-    fun updateCache(production: List<ProductionPayload>) {
-        production.map { it.origin }.stream().distinct().collect(Collectors.toList()).forEach {
-            productionCache[it] = production.filter { p -> p.origin == it }
-        }
+  private val productionCache = mutableMapOf<String, List<ProductionPayload>>()
+  private val productionQueue = mutableListOf<String>()
+  fun updateCache(production: List<ProductionPayload>) {
+    production.map { it.origin }.stream().distinct().collect(Collectors.toList()).forEach {
+      productionCache[it] = production.filter { p -> p.origin == it }
     }
-    fun setNext(origin: String) = productionQueue.add(origin)
-    fun getNext(): List<ProductionPayload>? =
-            runCatching { productionCache[productionQueue.removeFirstOrNull()] }.getOrDefault(null)
+  }
+  fun setNext(origin: String) = productionQueue.add(origin)
+  fun getNext(): List<ProductionPayload>? =
+      runCatching { productionCache[productionQueue.removeFirstOrNull()] }.getOrDefault(null)
 }
 
 fun Route.production() {
-    route("/production") {
-        get {
-            val production = queryProduction()
-            if (production.isEmpty()) call.respond(HttpStatusCode.OK, "Production orders empty")
-            updateCache(production)
-            val body = getNext()
-            if (body == null) call.respond(HttpStatusCode.OK, "Production orders empty")
-            else call.respond(HttpStatusCode.OK, body)
-        }
-        post {
-            val id = call.receive<IdPayload>()
-            val done = markAsDone(id.id)
-            if (done) call.respond(HttpStatusCode.OK, id.id)
-            else call.respond(HttpStatusCode.InternalServerError, "Production not marked as done")
-        }
+  route("/production") {
+    get {
+      val production = queryProduction()
+      if (production.isEmpty()) call.respond(HttpStatusCode.OK, "Production orders empty")
+      updateCache(production)
+      val body = getNext()
+      if (body == null) call.respond(HttpStatusCode.OK, "Production orders empty")
+      else call.respond(HttpStatusCode.OK, body)
     }
+    post {
+      val id = call.receive<IdPayload>()
+      val done = markAsDone(id.id)
+      if (done) call.respond(HttpStatusCode.OK, id.id)
+      else call.respond(HttpStatusCode.InternalServerError, "Production not marked as done")
+    }
+    post("/setNext") {
+      val uid = call.receive<String>()
+      setNext(uid)
+    }
+  }
 }
 
 @Serializable data class IdPayload(val id: Int)
