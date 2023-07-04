d1578bd4d1b870dab894bb438acc49c312847384
jesus <pablo@quadrosoluciones.mx>
2023-07-04 16:37:53 -0700

    aa

--- src/main/kotlin/com/coffee_service/quadro/org/model/Order.kt
+++ src/main/kotlin/com/coffee_service/quadro/org/model/Order.kt
@@ -2,17 +2,13 @@ package com.coffee_service.quadro.org.model
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class Order(
-    val uid: String,
-    val order_lines: List<OrderLine>
-)
+@Serializable data class Order(val uid: String, val orderlines: List<OrderLine>)
 
 @Serializable
 data class OrderLine(
     val product_id: Int,
     val options: ProductOptions,
-    val components: List<Components>
+    val extra_components: List<Components>
 )
 
 @Serializable data class Components(val product_id: Int, val qty: Int)
