5efc0e57f7fa36816fe27fff30b50c0c18114c2a
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2023-10-12 19:34:04 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
-# [Author: Jesus Alejos <https://github.com/jes-ale>]
+
+# Author: Jesus Alejos <https://github.com/jes-ale>
+# Script to stop, remove, build, and run a Docker container.
+
+# Define environment variables for container and image names
 CONTAINER="pos_coffee_container"
 IMAGE="pos_coffee_image"
-sudo docker stop $CONTAINER
-sudo docker rm $CONTAINER
+
+# Stop and remove the existing container (if it exists)
+if sudo docker ps -a | grep -q $CONTAINER; then
+    echo "Stopping and removing existing container: $CONTAINER"
+    sudo docker stop $CONTAINER
+    sudo docker rm $CONTAINER
+fi
+
+# Build the Docker image
+echo "Building Docker image: $IMAGE"
 sudo docker build -t $IMAGE .
+
+# Run the Docker container
+echo "Running Docker container: $CONTAINER"
 sudo docker run -d --env-file .env --net=host --name $CONTAINER $IMAGE
+
