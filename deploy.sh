4bfb8a57c19dae6d9605a8fe662348c8b88ce7c4
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2023-10-12 19:38:29 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
 # Author: Jesus Alejos <https://github.com/jes-ale>
-# Script to stop, remove, build, and run a Docker container.
+# Script to stop, remove, build, and run a Docker container only if the image has changed.
 
 # Define environment variables for container and image names
 CONTAINER="pos_coffee_container"
 IMAGE="pos_coffee_image"
 
-# Stop and remove the existing container (if it exists)
-if sudo docker ps -a | grep -q $CONTAINER; then
-    echo "Stopping and removing existing container: $CONTAINER"
-    sudo docker stop $CONTAINER
-    sudo docker rm $CONTAINER
+# Calculate the hash of the Dockerfile and source code
+DOCKERFILE_HASH=$(md5sum Dockerfile | awk '{print $1}')
+SOURCE_CODE_HASH=$(md5sum -c .source_code_hash 2>/dev/null | awk '{print $1}')
+
+# Calculate the image tag as a combination of Dockerfile and source code hashes
+IMAGE_TAG="$DOCKERFILE_HASH-$SOURCE_CODE_HASH"
+
+# Check if an image with the same tag exists
+if sudo docker image inspect $IMAGE:$IMAGE_TAG &>/dev/null; then
+    echo "Image $IMAGE:$IMAGE_TAG already exists. No changes detected."
+else
+    # Stop and remove the existing container (if it exists)
+    if sudo docker ps -a | grep -q $CONTAINER; then
+        echo "Stopping and removing existing container: $CONTAINER"
+        sudo docker stop $CONTAINER
+        sudo docker rm $CONTAINER
+    fi
+
+    # Build the Docker image with the specified tag
+    echo "Building Docker image: $IMAGE:$IMAGE_TAG"
+    sudo docker build -t $IMAGE:$IMAGE_TAG .
+
+    # Run the Docker container
+    echo "Running Docker container: $CONTAINER"
+    sudo docker run -d --env-file .env --net=host --name $CONTAINER $IMAGE:$IMAGE_TAG
 fi
-
-# Build the Docker image
-echo "Building Docker image: $IMAGE"
-sudo docker build -t $IMAGE .
-
-# Run the Docker container
-echo "Running Docker container: $CONTAINER"
-sudo docker run -d --env-file .env --net=host --name $CONTAINER $IMAGE
-
